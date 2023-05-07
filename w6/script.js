05358d7d8eab9fe2eda22cdd4df661c5f89ebf4b
Andrew Rockwell <ajrockwell@email.neit.edu>
2023-05-07 16:51:12 -0400

    assignment 6 (incomplete)

---
+++ w6/script.js
@@ -0,0 +1,29 @@
+var toggle = document.querySelector(`#toggle`)
+var tray = document.querySelector(`.tray`)
+
+var tabs = Array.from(document.querySelectorAll(`nav a`))
+var p = document.querySelector(`#editor p`)
+var flex2 = Array.from(document.querySelectorAll(`flex2`))
+
+toggle.addEventListener(`click`, (e)=>{
+    e.preventDefault();
+    tray.classList.toggle(`closed`)
+})
+
+for(let i=0; i<tabs.length; i++)
+{
+    tabs[i].addEventListener(`click`, selectTab);
+}
+
+function selectTab(e)
+{
+    for(let i=0; i<tabs.length; i++)
+    {
+        tabs[i].style.backgroundColor =`#373737`;
+    }
+    e.target.style.backgroundColor=`#161616`;
+    p.innerHTML =  `<a href=#>SE137</a> > 
+                    <a href=#>W6</a> >
+                    <a href=#>Homework</a> >
+                    <a href=#>Tab ${tabs.indexOf(e.target)+1}</a>`
+}
